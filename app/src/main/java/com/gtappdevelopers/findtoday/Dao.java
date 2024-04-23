180ddf4d72af6ce3b9b50b9ee3ead339851cadbe
NicoVitoriano001 <nicovitoriano@gmail.com>
2024-04-22 21:18:40 -0300

    indentei, tirei linha, limpei

--- app/src/main/java/com/gtappdevelopers/findtoday/Dao.java
+++ app/src/main/java/com/gtappdevelopers/findtoday/Dao.java
@@ -5,10 +5,8 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
-
 import java.util.List;
 
-//Adding annotation to our Dao class
 @androidx.room.Dao
 public interface Dao {
     @Insert
--- app/src/main/java/com/gtappdevelopers/findtoday/FinDatabase.java
+++ app/src/main/java/com/gtappdevelopers/findtoday/FinDatabase.java
@@ -14,7 +14,6 @@ public abstract class FinDatabase extends RoomDatabase {
     private static FinDatabase instance;
     public abstract Dao Dao();
 
-
     //on below line we are getting instance for our database.
     public static synchronized FinDatabase getInstance(Context context) {
         //below line is to check if the instance is null or not.
@@ -40,14 +39,11 @@ public abstract class FinDatabase extends RoomDatabase {
 
     //we are creating an async task class to perform task in background.
     private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
-
         PopulateDbAsyncTask(FinDatabase instance) {
             Dao dao = instance.Dao();
         }
-
         @Override
         protected Void doInBackground(Void... voids) {
-
             return null;
         }
     }
